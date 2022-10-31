c85983848a5aae315f7004217e07da6c110624fb
hajzeer <krzysh98@gmail.com>
2022-10-31 09:28:52 +0100

    edit config

---
+++ src/layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container">
+  <h2>
+    {{error.statusCode}} {{error.message}}
+  </h2>
+  <NuxtLink to="/">
+    Wróć do strony głównej
+  </NuxtLink>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: Elevon;
+  text-transform: uppercase;
+
+}
+</style>
