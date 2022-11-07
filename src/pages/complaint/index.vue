9c6921dbb09e2401de09738a3498617a8affe4a5
hajzeer <krzysh98@gmail.com>
2022-11-07 15:09:29 +0100

    privacy-policy

--- src/pages/complaint/index.vue
+++ src/pages/complaint/index.vue
@@ -2,7 +2,7 @@
   <div class="complaint">
     <h1 class="page-header__title">ZWROTY<br/> I REKLAMACJE</h1>
     <ol>
-      <li v-for="(text, index) in data" :key="text.text"><p>{{text.text}}<span v-if="index === 1">KLIKAJĄC TUTAJ</span></p></li>
+      <li v-for="(text, index) in data" :key="text.text"><p>{{text.text}}</p></li>
     </ol>
   </div>
 </template>
@@ -18,7 +18,7 @@ export default {
         {text: 'Zwracany towar nie może być uszkodzony lub zabrudzony. Jednakże może nosić wyłącznie normalne ślady użytkowania. Dodatkowo należy do niego dołączyć otrzymane opakowanie oraz fakturę. Zwrot towaru następuje na adres: YFAVC, ul. Piotrkowska 311/313, 90-980 Łódź, skr.poczt.57. Formularz zwrotu towaru pobierz '},
         {text: 'Zwrot pieniędzy następuję w ciągu 14 dni od dnia otrzymania przesyłki.'},
         {text: 'Produkty oferowane w sklepie są fabrycznie nowe.'},
-        {text: 'Klient może dokonać reklamacji poprzez formularz. Należy wydrukować go, wypełnić, a następnie wysłać go w paczce wraz ze zwracanym towarem na adres: YFAVC, ul. Piotrkowska 311/313, 90-980 Łódź, skr.poczt.57.'},
+        {text: 'Klient może dokonać reklamacji poprzez formularz. Należy wydrukować go, wypełnić, a następnie wysłać go w paczce wraz ze zwracanym towarem na adres: YFAVC, ul. Henryka Wieniawskiego 5A/28, 93-564 Łódź'},
         {text: 'Reklamacja zostanie rozpatrzona w ciągu 14 dni od otrzymania przesyłki zawierającej reklamowany produkt. O decyzji klient zostanie poinformowany drogą mailową.'},
       ]
     }
