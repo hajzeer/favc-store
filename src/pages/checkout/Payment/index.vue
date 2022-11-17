aea052f59b765585cfd995e572be28129f1c82cb
hajzeer <krzysh98@gmail.com>
2022-11-17 14:13:42 +0100

    parcelBox

--- src/pages/checkout/Payment/index.vue
+++ src/pages/checkout/Payment/index.vue
@@ -113,6 +113,9 @@
         </div>
         <div class="map__div" v-if="parcelBox === 1" :class="{active: parcelBox}">
           <inpost-geowidget onpoint='handleOnPoint' token='eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJzQlpXVzFNZzVlQnpDYU1XU3JvTlBjRWFveFpXcW9Ua2FuZVB3X291LWxvIn0.eyJleHAiOjE5ODQwMTkwMzQsImlhdCI6MTY2ODY1OTAzNCwianRpIjoiNDI4OTQ2MTItN2FlNC00NzE0LWJiYTctMjc4ODBjY2ZlZTU3IiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5pbnBvc3QucGwvYXV0aC9yZWFsbXMvZXh0ZXJuYWwiLCJzdWIiOiJmOjEyNDc1MDUxLTFjMDMtNGU1OS1iYTBjLTJiNDU2OTVlZjUzNTpVS0hobkFGZGFKTEZnOVYtakpZcGZBIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2hpcHgiLCJzZXNzaW9uX3N0YXRlIjoiM2JmNzNmOWQtMTZkNC00YWU5LTgyNTItOWFlMWU3NjQ2YjYyIiwic2NvcGUiOiJvcGVuaWQgYXBpOmFwaXBvaW50cyIsInNpZCI6IjNiZjczZjlkLTE2ZDQtNGFlOS04MjUyLTlhZTFlNzY0NmI2MiIsImFsbG93ZWRfcmVmZXJyZXJzIjoieWZhdmMuY29tLHd3dy55ZmF2Yy5jb20iLCJ1dWlkIjoiNGY5MGRkNzQtYzNkMS00NjM5LWFlYTItZjM2ZDIzMTUzODFlIn0.kkOF9DhdrEkkO_stiuC_82yh6aZwS83QVOIL35N-VtlU0C_kttmEAdQw7cwyz6GQsdnf7x7Aq72HwW68os3jI7JBVu3JUNCgkdabyGd70MrbmrMku-hOBE_E5-z30DvIUGd52EANp5mtgPz-wGO2j4aIiiLsCO3FO2NZwajzPhiKnrFPt4cqKZfNGdCscpswMGo5URsnd9x2F9IpUPtmedEo5uPIKRuzfcsrex1vIX68O3V3u-p19MTDJeCUydIRfAxs0svojhxw8opBRTj3J9Lq8_CHxKM9UflljALtJHZsU50RLNXMuExEYJZhS3kdL35aKXYhgz6B8BRbxdq9Ng' language='pl'  config='parcelCollect'></inpost-geowidget>
+          <p v-if="parcelBoxSelected">
+            TWÓJ PACZKOMAT: {{checkoutModel.customer.additionalInformation}}
+          </p>
         </div>
       </div>
 
@@ -195,10 +198,10 @@ export default {
 
     const checkoutModel = {
       basketModel: this.$store.state.basket.clientBasket,
+      additionalInformation: "" || "bez paczkomatu",
       customer: {
         firstName: initialFirstName || null,
         lastName: initialLastName || null,
-        additionalInformation: "" || "bez paczkomatu",
         addresses: [
           {
             type: "billing",
@@ -426,7 +429,7 @@ export default {
     },
     selectParcelBox(event) {
       this.parcelBoxSelected = true
-      this.checkoutModel.customer.additionalInformation = event.details.name
+      this.checkoutModel.additionalInformation = event.details.name
     },
   },
   mounted() {
