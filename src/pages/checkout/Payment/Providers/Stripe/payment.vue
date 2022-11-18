b1f969479ad3b7f8c8bee50d78834c8562a92d34
hajzeer <krzysh98@gmail.com>
2022-11-18 18:06:23 +0100

    normalizeOrderModel edit #6

--- src/pages/checkout/Payment/Providers/Stripe/payment.vue
+++ src/pages/checkout/Payment/Providers/Stripe/payment.vue
@@ -60,7 +60,7 @@ export default {
             },
           },
           metadata: {
-            PACZKOMAT: `${this.checkoutModel.customer.meta[0].value}`
+            "PACZKOMAT": `${this.checkoutModel.customer.meta[0].value}`
           }
         },
       },
